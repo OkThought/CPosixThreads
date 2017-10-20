9b6badaa645fd8db5961cdf9f15948a724268349
RoyPy <bogush.vano@gmail.com>
2017-10-20 10:42:17 +0700

    Edit error checking util
    Add formatted error message support
    Add const modifier where needed
    Add NO_MESSAGE and NO_ERROR macros
    Fix function names

--- util/headers/err_check.h
+++ util/headers/err_check.h
@@ -2,10 +2,19 @@
 
 #define NO_CLEANUP NULL
 #define NO_ARG NULL
+#define NO_MESSAGE NULL
+#define NO_ERROR 0
 
-void ExitIfNonZeroWithCleanupAndMessage(int code, void (*cleanup)(void *), void *arg, char *msg);
+void ExitIfNonZeroWithCleanupAndMessage(int code, void (*cleanup)(void *), void *arg, const char *msg);
+void ExitIfNonZeroWithCleanupAndFormattedMessage(int code, void (*cleanup)(void *), void *arg, const char *fmt, ...);
+void ExitIfNonZeroWithCleanup(int code, void (*cleanup)(void *), void *arg);
 void ExitIfNonZeroWithMessage(int code, char *msg);
+void ExitIfNonZeroWithFormattedMessage(int code, const char *fmt, ...);
 void ExitIfNonZero(int code);
-void ExitIfNullWithCleanupAndMessage(void *ptr, void (*cleanup)(void *), void *arg, char *msg);
-void ExitIfNullWithMessage(void *ptr, char *msg);
+
+void ExitIfNullWithCleanupAndMessage(void *ptr, void (*cleanup)(void *), void *arg, const char *msg);
+void ExitIfNullWithCleanupAndFormattedMessage(void *ptr, void (*cleanup)(void *), void *arg, const char *fmt, ...);
+void ExitIfNullWithCleanup(void *ptr, void (*cleanup)(void *), void *arg);
+void ExitIfNullWithMessage(void *ptr, const char *msg);
+void ExitIfNullWithFormattedMessage(void *ptr, const char *fmt, ...);
 void ExitIfNull(void *ptr);
--- util/src/err_check.c
+++ util/src/err_check.c
@@ -3,48 +3,117 @@
 #include <stdio.h> // fprintf
 #include <stdlib.h> // exit
 #include <string.h> // strerror
+#include <stdarg.h>
 
-void ExitIfNonZeroWithCleanupAndMessage(int code, void (*cleanup)(void *), void *arg, char *msg) {
-	if (code == 0) return;
+static void
+exit_cond_cleanup_fmt_v(int cond, int errcode, void (*cleanup)(void *), void *arg, const char *fmt, va_list list) {
+    if (!cond)
+        return;
 
-	if (cleanup != NO_CLEANUP) {
+    if (cleanup != NO_CLEANUP)
+        cleanup(arg);
+
+    if (fmt != NULL)
+        vfprintf(stderr, fmt, list);
+
+    if (errcode != NO_ERROR)
+        fprintf(stderr, ": %s\n", strerror(errcode));
+
+    exit(EXIT_FAILURE);
+}
+
+void
+ExitIfNonZeroWithCleanupAndMessage(int code, void (*cleanup)(void *), void *arg, const char *msg) {
+	if (code == 0)
+        return;
+
+	if (cleanup != NO_CLEANUP)
 		cleanup(arg);
-	}
-	if (msg != NULL) {
-	  fprintf(stderr, "%s: ", msg);
-	}
+
+	if (msg != NO_MESSAGE)
+        fprintf(stderr, "%s: ", msg);
+
 	fputs(strerror(code), stderr);
 	exit(EXIT_FAILURE);
 }
 
-void ExitIfNonZeroWithMessage(int code, char *msg) {
+void
+ExitIfNonZeroWithCleanupAndFormattedMessage(int code, void (*cleanup)(void *), void *arg, const char *fmt, ...) {
+    va_list list;
+    va_start(list, fmt);
+    exit_cond_cleanup_fmt_v((code != 0), code, cleanup, arg, fmt, list);
+    va_end(list);
+}
+
+void
+ExitIfNonZeroWithCleanup(int code, void (*cleanup)(void *), void *arg) {
+    ExitIfNonZeroWithCleanupAndMessage(code, cleanup, arg, NO_MESSAGE);
+}
+
+void
+ExitIfNonZeroWithMessage(int code, char *msg) {
 	ExitIfNonZeroWithCleanupAndMessage(code, NO_CLEANUP, NO_ARG, msg);
 }
 
-void ExitIfNonZero(int code) {
-	if (code != 0) {
+void
+ExitIfNonZeroWithFormattedMessage(int code, const char *fmt, ...) {
+    va_list list;
+    va_start(list, fmt);
+    exit_cond_cleanup_fmt_v((code != 0), code, NO_CLEANUP, NO_ARG, fmt, list);
+    va_end(list);
+}
+
+void
+ExitIfNonZero(int code) {
+	if (code != 0)
 		exit(EXIT_FAILURE);
-	}
 }
 
-void ExitIfNullWithCleanupAndMessage(void *p, void (*cleanup)(void *), void *arg, char *msg) {
-	if (p == NULL) {
-		if (cleanup != NO_CLEANUP) {
-			cleanup(arg);
-		}
-		if (msg != NULL) {
-			fputs(msg, stderr);
-		}
-		exit(EXIT_FAILURE);
-	}
+
+
+void
+ExitIfNullWithCleanupAndMessage(void *ptr, void (*cleanup)(void *), void *arg, const char *msg) {
+	if (ptr != NULL)
+        return;
+
+    if (cleanup != NO_CLEANUP)
+        cleanup(arg);
+
+    if (msg != NO_MESSAGE)
+        fputs(msg, stderr);
+
+    exit(EXIT_FAILURE);
 }
 
-void ExitIfNullWithMessage(void *p, char *msg) {
-	ExitIfNullWithCleanupAndMessage(p, NO_CLEANUP, NO_ARG, msg);
+void
+ExitIfNullWithCleanupAndFormattedMessage(void *ptr, void (*cleanup)(void *), void *arg, const char *fmt, ...) {
+    va_list list;
+    va_start(list, fmt);
+    exit_cond_cleanup_fmt_v((ptr == NULL), NO_ERROR, cleanup, arg, fmt, list);
+    va_end(list);
 }
 
-void ExitIfNull(void *p) {
-	if (p == NULL) {
+void
+ExitIfNullWithCleanup(void *ptr, void (*cleanup)(void *), void *arg) {
+    ExitIfNullWithCleanupAndMessage(ptr, cleanup, arg, NO_MESSAGE);
+}
+
+void
+ExitIfNullWithMessage(void *ptr, const char *msg) {
+	ExitIfNullWithCleanupAndMessage(ptr, NO_CLEANUP, NO_ARG, msg);
+}
+
+void
+ExitIfNullWithFormattedMessage(void *ptr, const char *fmt, ...) {
+    va_list list;
+    va_start(list, fmt);
+    exit_cond_cleanup_fmt_v((ptr == NULL), NO_ERROR, NO_CLEANUP, NO_ARG, fmt, list);
+    va_end(list);
+}
+
+void
+ExitIfNull(void *ptr) {
+	if (ptr == NULL) {
 		exit(EXIT_FAILURE);
 	}
 }
