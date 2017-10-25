deece93fa9d2e0d967f69952acaeafc8a6868844
RoyPy <bogush.vano@gmail.com>
2017-10-25 16:27:45 +0700

    Rewrite task7 totally
    - only main() is in main.c
    - all other task specific functions are in helpers.c

---
+++ task7/include/helpers.h
@@ -0,0 +1,26 @@
+#ifndef UTIL_HELPERS_H
+#define UTIL_HELPERS_H
+
+#include <pthread.h>
+
+#define SUCCESS 0
+#define DEFAULT_ATTRS NULL
+
+typedef struct {
+    int start_index;        // initial  value for i in the loop (including)
+    int finish_index;       // final    value for i in the loop (excluding)
+    double pi_part;         // partial sum
+} Payload;
+
+void         PrintUsage();
+int          ParseNumberOfThreads (const char *number_of_threads_string, int *number_of_threads, int max);
+void        *CalculatePI (void *);
+Payload*     ThreadPayloadsCreate (int number_of_threads);
+void         ThreadPayloadsDelete (void *payloads);
+void         ThreadPayloadsInit (Payload *payloads, int number_of_threads, int number_of_iterations);
+int          StartParallelPiCalculation(pthread_t *thread_ptr, int number_of_threads, const Payload *payloads);
+int          FinishParallelPiCalculation (pthread_t *thread_ptr, int number_of_threads, double *pi_ptr);
+
+
+
+#endif //UTIL_HELPERS_H
--- task7/src/main.c
+++ task7/src/main.c
@@ -1,90 +1,44 @@
 #include "err_check.h"
+#include "helpers.h"
 
 #include <stdio.h>          // printf puts
-#include <stdlib.h>         // strtol
-#include <limits.h>         // strtol
-#include <errno.h>          // errno
-#include <pthread.h>        // pthread_*
+#include <stdlib.h>         // exit
 #include <math.h>           // M_PI
 
-#define ITERATIONS_NUMBER 200000000 // 2 * 10^8
-#define DECIMAL_BASE 10
+static const int NUMBER_OF_ITERATIONS = 200000000; // 2 * 10^8
+static const int REQUIRED_NUMBER_OF_ARGUMENTS = 2;
+static const int MAX_NUMBER_OF_THREADS = 100;
 
-void *CalculatePI(void *);
-
-typedef struct Payload {
-    int start_index;        // initial  value for i in the loop (including)
-    int finish_index;       // final    value for i in the loop (excluding)
-} Payload;
-
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("Usage: %s number_of_threads\n", argv[0]);
-        puts("\tnumber_of_threads - number of threads to run execution on");
+int main (int argc, char **argv) {
+    if (argc != REQUIRED_NUMBER_OF_ARGUMENTS) {
+        PrintUsage();
         exit(EXIT_FAILURE);
     }
 
-    errno = 0;
-    long number_of_threads_long = strtol(argv[1], NULL, DECIMAL_BASE);
-    ExitIfNonZeroWithFormattedMessage(errno, "Couldn't convert '%s' to int", argv[1]);
+    int number_of_threads;
+    ParseNumberOfThreads (argv[1], &number_of_threads, MAX_NUMBER_OF_THREADS);
 
-    ExitIfTrueWithErrcodeAndMessage(number_of_threads_long <= 0 || number_of_threads_long > INT_MAX, EINVAL,
-                                    "number_of_threads must be positive and must fit into int size");
+    Payload *payloads = ThreadPayloadsCreate (number_of_threads);
+    ExitIfNullWithFormattedMessage ((void *) payloads, "Couldn't allocate %d payloads each of size %dB (total %dB)",
+                                    number_of_threads, sizeof (Payload), sizeof (Payload) * number_of_threads);
 
-    int number_of_threads = (int) number_of_threads_long;
+    ThreadPayloadsInit (payloads, number_of_threads, NUMBER_OF_ITERATIONS);
 
     pthread_t threads[number_of_threads];
-    Payload *payloads = (Payload *) malloc(sizeof (Payload) * number_of_threads);
-    ExitIfNullWithFormattedMessage((void *) payloads, "Couldn't allocate %d payloads each of size %dB (total %dB)",
-                                   number_of_threads, sizeof (Payload), sizeof (Payload) * number_of_threads);
+    int code = StartParallelPiCalculation (threads, number_of_threads, payloads);
+    ExitIfNonZeroWithCleanupAndMessage (code, ThreadPayloadsDelete, payloads,
+                                       "Error on start parallel pi calculation");
 
-    int iterations_per_thread = ITERATIONS_NUMBER / number_of_threads;
-    int iterations_rest = ITERATIONS_NUMBER - number_of_threads * iterations_per_thread;
-    int prev_begin = 0;
+    double pi;
+    code = FinishParallelPiCalculation (threads, number_of_threads, &pi);
+    ExitIfNonZeroWithCleanupAndMessage (code, ThreadPayloadsDelete, payloads,
+                                       "Error on finish parallel pi calculation");
 
-    int code;
-    int i;
-    for (i = 0; i < number_of_threads; ++i) {
-        payloads[i].start_index = prev_begin;
-        prev_begin = payloads[i].finish_index = prev_begin + iterations_per_thread + (i < iterations_rest);
+    ThreadPayloadsDelete (payloads);
 
-        code = pthread_create(threads + i, NULL, CalculatePI, (void *) (payloads + i));
-        ExitIfNonZeroWithFormattedMessage(code, "Couldn't create thread #%d", i);
-    }
-
-    double pi = 0.;
-    double *pi_part_ptr;
-    void *status;
-
-    for (i = 0; i < number_of_threads; ++i) {
-        code = pthread_join(threads[i], &status);
-        ExitIfNonZeroWithFormattedMessage(code, "Couldn't join thread #%d", i);
-
-        pi_part_ptr = (double *) status;
-        pi += *pi_part_ptr;
-        free(pi_part_ptr);
-    }
-
-
-    pi *= 4.;
-    printf("pi done - %.15g \n", pi);
-    printf("actual  - %.15g \n", M_PI);
+    printf ("pi done - %.15g \n", pi);
+    printf ("actual  - %.15g \n", M_PI);
 
     exit (EXIT_SUCCESS);
 }
 
-void *CalculatePI(void *arg) {
-    Payload *payload = (Payload *) arg;
-    int i;
-    double pi_part = 0;
-    for (i = payload->start_index; i < payload->finish_index; i++) {
-        pi_part += 1.0/(i*4.0 + 1.0);
-        pi_part -= 1.0/(i*4.0 + 3.0);
-    }
-
-    double *pi_part_ptr = (double *) malloc(sizeof (double));
-    ExitIfNullWithFormattedMessage(pi_part_ptr, "Couldn't allocate %dB for pi_part_ptr", sizeof (double));
-
-    *pi_part_ptr = pi_part;
-    pthread_exit((void *) pi_part_ptr);
-}
