cb0c5c677f54aa98f87a00bd5e126c8c6dcd51cb
RoyPy <bogush.vano@gmail.com>
2017-12-05 12:06:15 +0700

    Cleanup code

--- task12/src/main.c
+++ task12/src/main.c
@@ -31,12 +31,9 @@ PrintCount (enum Entity executingEntity, const char *name, int from, int to) {
     const enum Entity waitingEntity = executingEntity == PARENT ? CHILD : PARENT;
 
     for (count = from; count <= to; ++count) {
-        // EINVAL:       The value specified by mutex is invalid. (will not happen)
-        // EDEADLK:     A deadlock would occur if the thread blocked waiting for mutex. (will not happen)
         (void) pthread_mutex_lock (&mutex);
 
         while (printingEntity != executingEntity) {
-            // EINVAL:  The value specified by cond or the value specified by mutex is invalid. (will not happen)
             (void) pthread_cond_wait (&cond, &mutex);
         }
 
@@ -44,8 +41,6 @@ PrintCount (enum Entity executingEntity, const char *name, int from, int to) {
 
         printingEntity = waitingEntity ;
 
-        // EINVAL:  The value specified by mutex is invalid. (will not happen)
-        // EPERM:   The current thread does not hold a lock on mutex. (will not happen)
         (void) pthread_mutex_unlock (&mutex);
     }
 }
@@ -62,26 +57,24 @@ InitializeResources () {
 
     code = pthread_mutexattr_init (&mutexattr);
     if (code != SUCCESS) {
-        fputs ("Couldn't init mutex attributes object\n", stderr);
+        (void) fputs ("Couldn't init mutex attributes object\n", stderr);
         return code;
     };
 
-    // EINVAL:   The value specified either by type or attr is invalid. (will not happen)
     (void) pthread_mutexattr_settype (&mutexattr, PTHREAD_MUTEX_ERRORCHECK);
 
     code = pthread_mutex_init (&mutex, &mutexattr);
     if (code != SUCCESS) {
-        fputs ("Couldn't init mutex\n", stderr);
+        (void) fputs ("Couldn't init mutex\n", stderr);
         return code;
     };
 
     code = pthread_cond_init (&cond, DEFAULT_ATTR);
     if (code != SUCCESS) {
-        fputs ("Couldn't init cond\n", stderr);
+        (void) fputs ("Couldn't init cond\n", stderr);
         return code;
     };
 
-    // EINVAL:  Invalid value for attr. (ignore)
     (void) pthread_mutexattr_destroy (&mutexattr);
 
     return SUCCESS;
@@ -93,11 +86,11 @@ DestroyResources () {
 
     code = pthread_mutex_destroy (&mutex);
     if (code != SUCCESS)
-        fprintf (stderr, "Couldn't destroy mutex: %s\n", strerror (code));
+        (void) fprintf (stderr, "Couldn't destroy mutex: %s\n", strerror (code));
 
     code = pthread_cond_destroy (&cond);
     if (code != SUCCESS)
-        fprintf (stderr, "Couldn't destroy cond: %s\n", strerror (code));
+        (void) fprintf (stderr, "Couldn't destroy cond: %s\n", strerror (code));
 
     return code;
 }
@@ -113,24 +106,18 @@ main (int argc, char **argv) {
     code = pthread_create (&child_thread, DEFAULT_ATTR, RunChild, NO_ARG);
     if (code != SUCCESS) {
         (void) DestroyResources ();
-        fputs ("Couldn't start child_thread\n", stderr);
+        (void) fputs ("Couldn't start child_thread\n", stderr);
         exit (EXIT_FAILURE);
     };
 
     PrintCount (PARENT, "Parent", COUNT_FROM, COUNT_TO);
 
-    // EINVAL:   The implementation has detected that the value specified by thread does not refer to a joinable thread.
-    // (will not happen)
-    // ESRCH:    No thread could be found corresponding to that specified by the given thread ID, thread.
-    // (will not happen)
-    // EDEADLK:  A deadlock was detected or the value of thread specifies the calling thread.
-    // (will not happen)
     (void) pthread_join (child_thread, IGNORE_STATUS);
 
     code = DestroyResources ();
     if (code != SUCCESS) {
         exit_status = EXIT_FAILURE;
-        fputs ("Error in DestroyResources\n", stderr);
+        (void) fputs ("Error in DestroyResources\n", stderr);
     };
 
     exit (exit_status);
